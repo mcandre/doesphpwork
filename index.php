256b9b59ecfe7482831f4d8074564cddc1cc29c1
Andrew Pennebaker <Andrew Pennebaker>
2011-03-10 17:30:19 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Does PHP Work?</title>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	<link rel="favorites icon" href="/favicon.ico" />
+</head>
+<body>
+<p><a href="https://github.com/mcandre/doesphpwork">GitHub</a></p>
+<h1>Does PHP Work?</h1>
+<p>You betcha!</p>
+<h1>Implementation</h1>
+<p>PHP <?php echo phpversion(); ?></p>
+</body>
+</html>
